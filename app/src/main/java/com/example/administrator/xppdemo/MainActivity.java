af9f86ba5578d356602e31dc8cc66f5c9681c6f3
xpp <1522374636@qq.com>
2016-07-24 21:18:30 +0800

    1. 我今天修改了代码，增加了toast部分。

--- app/src/main/java/com/example/administrator/xppdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/xppdemo/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Toast.makeText(getApplicationContext(),"abcd",Toast.LENGTH_SHORT).show();
             }
         });
 
